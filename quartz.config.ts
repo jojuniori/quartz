890e9a14a34f9921ec4049bf7005b5fa2089547b
JoJunIori <i@neko0.com>
2025-07-02 10:13:58 +0800

    update

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "jojuniori.github.io/quartz",
+    baseUrl: "notes.neko0.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
