e42ad64f80690083d6bf4bee98a9f7605b3461bf
乔弥月 <i@neko0.com>
2025-07-02 09:07:18 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,5 +1,7 @@
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
+import fs from "fs"
+const alibabaFont = fs.readFileSync("./quartz/static/AlibabaPuHuiTi-3-55-Regular.ttf")
 
 /**
  * Quartz 4 Configuration
@@ -98,7 +100,45 @@ const config: QuartzConfig = {
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
+      Plugin.CustomOgImages({
+        // 3. 传入字体配置
+        fonts: [
+          {
+            name: "Alibaba PuHuiTi", // 在 CSS 中使用的字体名称
+            data: alibabaFont,       // 读取到的字体文件数据
+            weight: 400,
+            style: "normal",
+          },
+        ],
+        // 4. 修改 OG 图片的 React 组件
+        Component: (props) => (
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              // 使用你配置的亮色主题
+              backgroundColor: "#faf8f8",
+              color: "#2b2b2b",
+              // 直接使用上面定义的字体名称
+              fontFamily: '"Alibaba PuHuiTi"',
+            }}
+          >
+            <h1 style={{ fontSize: "60px", fontWeight: 500, marginBottom: "20px", textAlign: 'center', padding: '0 40px' }}>
+              {props.title}
+            </h1>
+            <p style={{ fontSize: "28px", margin: 0, textAlign: 'center', padding: '0 40px', color: '#4e4e4e' }}>
+              {props.description}
+            </p>
+            <p style={{ position: 'absolute', bottom: '40px', fontSize: '24px', color: '#b8b8b8' }}>
+              {props.options.configuration.pageTitle}
+            </p>
+          </div>
+        ),
+      }),
     ],
   },
 }
